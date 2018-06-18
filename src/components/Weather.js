6fde073a2e939a4f1481f4d1ccb87328a1fd95df
weitao1994 <40348766+weitao1994@users.noreply.github.com>
2018-06-18 01:08:14 -0700

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -13,33 +13,7 @@ class Weather extends React.Component {
                     this.props.temperature && <p className="weather__key">Temperature:
                     <span className="weather__value"> {this.props.temperature}°C </span></p>
                 }
-                {
-                    this.props.totalSnowfall_cm && <p className="weather__key"> Today's snowfall:
-                    <span className="weather__value"> {this.props.totalSnowfall_cm} cm </span></p>
-                }
-                {
-                    this.props.chance_of_snow && <p className="weather__key">Chance of snow:
-                    <span className="weather__value"> {this.props.chance_of_snow}% </span></p>
-                }
-                {
-                    this.props.chance_of_fog && <p className="weather__key">Chance of fog:
-                    <span className="weather__value"> {this.props.chance_of_fog}% </span></p>
-                }
-                {
-                    this.props.visibility && <p className="weather__key">Visibility:
-                    <span className="weather__value"> Level {this.props.visibility} </span></p>
-                }
-                {
-                    this.props.humidity && <p className="weather__key">Humidity:
-                    <span className="weather__value"> {this.props.humidity}% </span></p>
-                }
-                {
-                    this.props.wind_speed && <p className="weather__key">Wind speed:
-                    <span className="weather__value"> {this.props.wind_speed} km/h </span></p>
-                }
-                {
-                    this.props.error && <p className="weather__error"> {this.props.error} </p>
-                }
+              
             </div>
         );
     }
